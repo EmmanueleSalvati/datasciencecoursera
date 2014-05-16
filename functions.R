4473821d949a6dc317aae070232c4f7678ccca3c
Emmanuele <emmanuele.salvati@gmail.com>
2014-05-16 18:10:20 -0400

    gitignore

--- functions.R
+++ functions.R
@@ -1,22 +1,37 @@
 add2 <- function(x, y) {
-  +   x + y
-  + }
+    +   x + y
+    + }
 
 above10 <- function(x) {
-  use <- x > 10
-  x[use]
+    use <- x > 10
+    x[use]
 }
 
 above <- function(x, n = 10) {
-  use <- x > n
-  x[use]
+    use <- x > n
+    x[use]
 }
 
 columnmean <- function(y, removeNA = TRUE) {
-  nc <- ncol(y)
-  means <- numeric(nc)
-  for(i in 1:nc) {
-    means[i] <- mean(y[, i], na.rm = removeNA)
-  }
-  means
+    nc <- ncol(y)
+    means <- numeric(nc)
+    for(i in 1:nc) {
+        means[i] <- mean(y[, i], na.rm = removeNA)
+    }
+    means
+}
+
+f <- function(x) {
+    g <- function(y) {
+        y + z
+    }
+    z <- 4
+    x + g(x)
+}
+
+x <- 5
+y <- if(x < 3) {
+    NA
+} else {
+    10
 }
