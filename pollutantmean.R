b28229767cfe94f7ae193d403818e7256f4a2a84
Emmanuele <emmanuele.salvati@gmail.com>
2014-05-17 13:01:54 -0400

    Complete pollutantmean.R

--- pollutantmean.R
+++ pollutantmean.R
@@ -4,34 +4,36 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
                 input_dir <- dir(directory)
                 df <- data.frame()
                 file_number <- 1
-                #         while (file_number <= length(input_dir)) {
-                while (file_number <= 5) {
+                while (file_number <= length(input_dir)) {
                         tmp_csv_file <- paste(directory, input_dir[file_number], sep = "")
                         tmp_df <- read.csv(tmp_csv_file)
                         df <- rbind(df, tmp_df)
                         file_number <- file_number + 1
+                        rm(tmp_df)
+                        rm(tmp_csv_file)
                 }
                 df
         }
         
-        # 2. Get the mean in a column
-        get_mean <- function(dataframe, pollutant, id = 1) {
-                condition <- dataframe$ID == id
-                column <- dataframe[condition, pollutant]
-                mean(column, na.rm = TRUE)
+        # 2. Slice the data frame by id
+        get_by_id <- function(dataframe, monitors_vector = 1) {
+            df <- data.frame()
+            for(monitor in monitors_vector) {
+                tmp_df <- dataframe[dataframe$ID == monitor, ]
+                df <- rbind(df, tmp_df)
+            }
+            df
         }
         
+        # 3. Get the mean in a column
+        get_mean <- function(dataframe, pollutant) {
+                pollutant_column <- dataframe[, pollutant]
+                mean(pollutant_column, na.rm = TRUE)
+        }
+        
+        # 4. Now use the functions
         df <- get_data_frames(directory)
-        # get_mean(df, pollutant, id)
-
-        v_ID <- df[, "ID"]
-        print(class(v_ID))
-        sel_ID <- v_ID[v_ID == id]
-        print(length(id))
-        print(v_ID)
-        # print(class(condition))
-        # nrow(df[condition, c(pollutant, "ID")])
-        # print(id)
-        # my_cond <- c(df$ID[df$ID == id])
-        # nrow(df[my_cond, pollutant])
+        selected_monitors_df <- get_by_id(df, id)
+        get_mean(selected_monitors_df, pollutant)
+        
 }
